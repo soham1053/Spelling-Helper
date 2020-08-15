71d8dcac3a349a78aa10ac4d11da1f28bf2139f3
SatvikR <49799352+SatvikR@users.noreply.github.com>
2020-08-15 12:23:57 -0700

    moving to soham

--- scratchpad.py
+++ scratchpad.py
@@ -1,9 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
+import cv2
 
-image = Image.open("temp.png")
-np_img = np.asarray(image)
+img = cv2.imread('temp.png')
+res = cv2.resize(img, dsize=(28, 28))
+np_img = np.asarray(res)
+np_img = (np_img/255) - 0.4
+np_img = np.ceil(np_img).astype(int)
+np_img = np_img*255
 
 plt.imshow(np_img, cmap='Greys')
 plt.show()
