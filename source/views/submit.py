67fc5bd8bf366d69f0e9306bbfe6f952409cc7b4
Soham <soham1053@gmail.com>
2020-08-15 17:04:56 -0700

    Deployed Model

--- source/views/submit.py
+++ source/views/submit.py
@@ -4,8 +4,11 @@ from PIL import Image
 import base64
 import re
 import numpy as np
-import matplotlib.pyplot as plt
 from flask import request
+import cv2
+from tensorflow.keras.models import load_model
+
+model = load_model('letters_model')
 
 @app.route("/submit", methods=["POST"])
 def submit():
@@ -13,9 +16,16 @@ def submit():
 	image_data = re.sub('^data:image/.+;base64,', '', image_b64)
 	image_data = base64.decodestring(image_data.encode())
 	image_PIL = Image.open(io.BytesIO(image_data))
-	image_PIL.save("temp.png")
-	image_np = np.array(image_PIL)
-	plt.imshow(image_np, cmap='Greys')
+	res = image_PIL.resize((28, 28))
+	np_img = np.asarray(res)
+	np_img = (np_img/255) - 0.4
+	np_img = np.ceil(np_img).astype(int)
+	np_img = np_img*255
+	np_img = abs(np_img - 255)
 
-	print('Image received: {}'.format(image_np.shape)) # Prints out: Image received: (500, 500, 4)
-	return "done"
+	pred = chr(model.predict(np_img[:,:,0].reshape(1, 28, 28, 1)).argmax()+97)
+
+	print(f"Predicted Letter: {pred}")
+
+	# print('Image received: {}'.format(image_np.shape)) # Prints out: Image received: (500, 500, 4)
+	return pred
